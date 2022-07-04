613c2ff0ce2bca5ad26e627997cacf557deaf7d6
wpzou <97920123+wpzou@users.noreply.github.com>
2022-07-05 08:25:38 +0900

    S4.完了

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,12 +1,30 @@
-export const ChildArea = (props) => {
-  const { open } = props;
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildAreaがレンダリングされた!!");
+
+  const data = [...Array(3000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
+  console.log(data);
+
   return (
     <>
       {open ? (
-        <div>
+        <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
