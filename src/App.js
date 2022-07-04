ace29af613e79b06776cb70485d7113b2ff035b7
wpzou <97920123+wpzou@users.noreply.github.com>
2022-07-05 06:53:58 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <p>{count}</p>
+    </div>
+  );
+}
